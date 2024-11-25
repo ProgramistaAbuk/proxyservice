c258a725525577c21ea6718fd873322f489ffe9b
ProgramistaAbuk <jakob.szatan@gmail.com>
2024-11-24 22:31:43 -0500

    Initial Commit
    Commit 1

---
+++ src/test/java/ca/abuk/proxy/ProxyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.abuk.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
